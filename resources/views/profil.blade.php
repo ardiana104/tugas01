ae9b5dd2a86bfef5d995b71771da2a10c0facb8c
ardiana104 <kadekardiana201@gmail.com>
2022-03-06 19:21:06 +0800

    jetspulsa

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.main')
+
+@section('toko')
+<div class="card bg-dark text-white">
+    <img src="img/jet.jpg" class="card-img" width="" alt="jet">
+    <div class="card-img-overlay">
+      <h4 class="card-name">JETS CELLULAR</h4>
+      <h5 class="card-name">jetsmartphone@gmail.com</h5>
+      <p class="card-text">Siap Melayani</p>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header">
+
+    </div>
+    <div class="card-body text-center">
+      <h5 class="card-tetx">Terima Kasih Telah Berkunjung</h5>
+      <p class="card-text">Info Selengkapnya Tentang Toko Ada Di Bawah Ini</p>
+      <a href="#" class="btn btn-primary">Readmore </a>
+    </div>
+  </div>
+@endsection
